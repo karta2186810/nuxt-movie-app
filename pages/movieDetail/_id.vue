f3ea2049be122b5d1c553d6d1d8cece3a1266c7c
karta2186810 <karta2186810@gmail.com>
2021-10-04 20:51:26 +0800

    feat: 獲取電影詳情邏輯

--- pages/movieDetail/_id.vue
+++ pages/movieDetail/_id.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="movie-detail"></div>
+  <div class="movie-detail">
+    {{ movie.title }}
+  </div>
 </template>
 
 <script>
+import http from '@/utils/http.js'
 export default {
   name: 'MovieDetail',
+  data() {
+    return {
+      movie: {},
+    }
+  },
+  async fetch() {
+    const result = await http.get(`movie/${this.$route.params.id}`)
+    this.movie = result.data
+  },
+  computed: {
+    genres() {
+      return this.$store.getters.getGenres
+    },
+  },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.movie-detail {
+  margin-top: $nav-height;
+}
+</style>
